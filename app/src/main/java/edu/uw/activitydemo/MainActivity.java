2443087092ab435a1a83c4447be7cbf451df47c3
Joel Ross <joelross@uw.edu>
2017-09-12 07:07:33 -0700

    Reset for start of lecture

--- app/src/main/java/edu/uw/activitydemo/MainActivity.java
+++ app/src/main/java/edu/uw/activitydemo/MainActivity.java
@@ -1,90 +1,13 @@
 package edu.uw.activitydemo;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MAIN";
-    private static final String MSG_KEY = "message_key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = (Button)findViewById(R.id.my_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.v(TAG, "You clicked me!");
-
-                Toast.makeText(MainActivity.this, "Starting second activity", Toast.LENGTH_LONG).show();
-
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Log.d(TAG, "onCreate() called");
-
-        if(savedInstanceState != null){
-            Log.v(TAG, "Recreated");
-            Log.v(TAG, savedInstanceState.getString(MSG_KEY));
-        }
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        Log.d(TAG, "onStart() called");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        Log.d(TAG, "onResume() called");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        Log.d(TAG, "onPause() called");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        Log.d(TAG, "onStop() called");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-
-        Log.d(TAG, "onRestart() called");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        Log.d(TAG, "onDestroy() called");
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        outState.putString(MSG_KEY, "Hello world"); //store a message
-
-        super.onSaveInstanceState(outState);
     }
 }
